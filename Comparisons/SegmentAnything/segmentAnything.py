122a75daa7bc1873366faec984b91614040925a0
Mona Wang <monaw7777@gmail.com>
2025-05-21 15:20:20 -0400

    testing one img

--- Comparisons/SegmentAnything/segmentAnything.py
+++ Comparisons/SegmentAnything/segmentAnything.py
@@ -1,9 +1,16 @@
-! pip install git+https://github.com/facebookresearch/segment-anything.git
+import os
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
-from segment_anything import SamPredictor, sam_model_registry
+img_path = os.path.join('.', 'img', 'Sample_1-1.jpg')
+checkpoint_path = os.path.join('.', 'sam_vit_h_4b8939.pth')
 
+sam = sam_model_registry["default"](checkpoint=checkpoint_path)
+mask_generator = SamAutomaticMaskGenerator(sam)
+masks = mask_generator.generate(img_path)
 
-sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
-predictor = SamPredictor(sam)
-predictor.set_image(<your_image>)
-masks, _, _ = predictor.predict(<input_prompts>)
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+plt.axis('off')
+plt.savefig(os.path.join('.', 'output', 'out_Sample_1-1.jpg'))
+
+plt.close()
